bcf9ebfd73624a757e27bdd7ff2337cc6e6d597d
Sam Claus <samclaus@protonmail.com>
2024-01-14 15:30:58 -0500

    Rename "Login" component to "Auth"
    "Auth" is more correct because the component handles registration in addition to login.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     import { SESSION$ } from "./session";
-    import Login from "./ui/Login.svelte";
+    import Auth from "./ui/Auth.svelte";
     import Main from "./ui/Main.svelte";
 </script>
 
 {#if $SESSION$.state === "logged-in"}
     <Main conn={$SESSION$.conn} firstLogin={$SESSION$.firstLogin} />
 {:else}
-    <Login />
+    <Auth />
 {/if}
