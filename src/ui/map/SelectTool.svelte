665fadba6814b52aeddec98dac5a7d4907336db5
Sam Claus <samclaus@protonmail.com>
2024-02-09 12:44:50 -0500

    Clean up select tool wording and styles

--- src/ui/map/SelectTool.svelte
+++ src/ui/map/SelectTool.svelte
@@ -7,8 +7,57 @@
 
     export let map: L.Map;
 
+    const enum FeatureType {
+        Stop,
+        Polyline,
+        Polygon,
+        Circle,
+    }
+    const FEATURE_TYPES: { readonly [Type in FeatureType]: string } = [
+        "Stop",
+        "Polyline",
+        "Polygon",
+        "Circle",
+    ];
+    type FeatureModel = [FeatureType, string, string];
+
+    let features: FeatureModel[] = [];
+
+    $: {
+        const f = $PROJECT_FEATURES;
+
+        features.length = 0; // TODO: does this cause additional Svelte work?
+
+        for (const s of f.stops) {
+            features.push([FeatureType.Stop, s.id, s.name]);
+        }
+        for (const p of [...f.paths].sort((l, r) => (+l.line) - (+r.line))) {
+            features.push([
+                p.line ? FeatureType.Polyline : FeatureType.Polygon,
+                p.id,
+                p.name,
+            ]);
+        }
+        for (const c of f.circles) {
+            features.push([FeatureType.Circle, c.id, c.name]);
+        }
+
+        features = features;
+    };
+
     function onMapClick({ latlng }: any): void {
         // TODO
+    }
+
+    function onDeleteClick(type: FeatureType, id: string): void {
+        // TODO: error handling
+        if (type === FeatureType.Stop) {
+            deleteStop(id);
+        } else if (type === FeatureType.Polyline || type === FeatureType.Polygon) {
+            deletePath(id);
+        } else if (type === FeatureType.Circle) {
+            deleteCircle(id);
+        }
     }
 
     map.on("click", onMapClick);
@@ -20,47 +69,54 @@
     });
 </script>
 
+{#if !features.length}
 <div class="placeholder">
     <Icon
         name="cursor"
         color="primary"
         size={72} />
-    <h3>Select Tool</h3>
+    <h3>Nothing to Select</h3>
+    <p>
+        This project does not contain any map features yet. Try using one
+        of the tools above to create some stops or geometry on the map.
+    </p>
+</div>
+{:else}
+<div class="padding">
     <p>
         Click a stop, polygon, or other feature on the map to view and
         edit it. You can select a set of features by holding <kbd>⌘</kbd>
         or <kbd>Ctrl</kbd> while you click and drag the mouse on the map.
+        Or you can select features below.
     </p>
     <ul>
-        {#each $PROJECT_FEATURES.stops as stop (stop.id)}
-            <li>
-                {stop.name}
-                <IconButton
-                    label="Delete"
-                    icon="delete"
-                    color="warn"
-                    on:click={() => deleteStop(stop.id)} />
-            </li>
-        {/each}
-        {#each $PROJECT_FEATURES.paths as path (path.id)}
-            <li>
-                {path.name}
-                <IconButton
-                    label="Delete"
-                    icon="delete"
-                    color="warn"
-                    on:click={() => deletePath(path.id)} />
-            </li>
-        {/each}
-        {#each $PROJECT_FEATURES.circles as circle (circle.id)}
-            <li>
-                {circle.name}
-                <IconButton
-                    label="Delete"
-                    icon="delete"
-                    color="warn"
-                    on:click={() => deleteCircle(circle.id)} />
+        {#each features as [type, id, name] (id)}
+            <li class="card">
+                <h3>{name}</h3>
+                <h4>{FEATURE_TYPES[type]}</h4>
+                <div class="card-actions">
+                    <IconButton
+                        label="Delete"
+                        icon="delete"
+                        color="warn"
+                        on:click={() => onDeleteClick(type, id)} />
+                </div>
             </li>
         {/each}
     </ul>
 </div>
+{/if}
+
+<style>
+    .padding {
+        padding: 0 48px;
+    }
+
+    ul {
+        padding: 0;
+    }
+
+    li {
+        margin: 24px 0;
+    }
+</style>
